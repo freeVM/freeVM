2c7be875be70c05026faa2cde36d7152b7fa3e97
Nathan Beyer <apache@apache.org>
2006-07-22 04:19:37 +0000

    Add serialization method stubs.
    svn path=/incubator/harmony/; revision=424520

--- standard/sandbox/juc-proposal/concurrent/src/main/java/java/util/concurrent/CopyOnWriteArrayList.java
+++ standard/sandbox/juc-proposal/concurrent/src/main/java/java/util/concurrent/CopyOnWriteArrayList.java
@@ -1,20 +1,24 @@
-/* Copyright 2006 The Apache Software Foundation or its licensors, as applicable
+/*
+ * Copyright 2006 The Apache Software Foundation or its licensors, as applicable
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  * 
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package java.util.concurrent;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Iterator;
@@ -25,19 +29,19 @@ import java.util.RandomAccess;
 public class CopyOnWriteArrayList<E> implements List<E>, RandomAccess, Cloneable, Serializable {
 
     private static final long serialVersionUID = 8673264195747942595L;
-    
+
     public CopyOnWriteArrayList() {
         super();
     }
-    
+
     public CopyOnWriteArrayList(Collection<? extends E> c) {
         super();
     }
-    
+
     public CopyOnWriteArrayList(E[] array) {
         super();
     }
-    
+
     public boolean add(E e) {
         return false;
     }
@@ -52,18 +56,18 @@ public class CopyOnWriteArrayList<E> implements List<E>, RandomAccess, Cloneable
     public boolean addAll(int index, Collection<? extends E> c) {
         return false;
     }
-    
+
     public int addAllAbsent(Collection<? extends E> c) {
         return 0;
     }
-    
+
     public boolean addIfAbsent(E e) {
         return false;
     }
 
     public void clear() {
     }
-    
+
     public Object clone() {
         return null;
     }
@@ -75,7 +79,7 @@ public class CopyOnWriteArrayList<E> implements List<E>, RandomAccess, Cloneable
     public boolean containsAll(Collection<?> c) {
         return false;
     }
-    
+
     public boolean equals(Object o) {
         return false;
     }
@@ -83,7 +87,7 @@ public class CopyOnWriteArrayList<E> implements List<E>, RandomAccess, Cloneable
     public E get(int index) {
         return null;
     }
-    
+
     public int hashCode() {
         return 0;
     }
@@ -91,7 +95,7 @@ public class CopyOnWriteArrayList<E> implements List<E>, RandomAccess, Cloneable
     public int indexOf(E e, int index) {
         return 0;
     }
-    
+
     public int indexOf(Object o) {
         return 0;
     }
@@ -107,7 +111,7 @@ public class CopyOnWriteArrayList<E> implements List<E>, RandomAccess, Cloneable
     public int lastIndexOf(E e, int index) {
         return 0;
     }
-    
+
     public int lastIndexOf(Object o) {
         return 0;
     }
@@ -158,5 +162,13 @@ public class CopyOnWriteArrayList<E> implements List<E>, RandomAccess, Cloneable
 
     public String toString() {
         return null;
+    }
+
+    private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+
+    }
+
+    private void writeObject(ObjectOutputStream s) throws IOException {
+
     }
 }
