2f03bb21f267305aff20745d432f96c605d6a938
Alexey Varlamov <apache@apache.org>
2006-11-21 06:23:40 +0000

    Applied HARMONY-2207 [drlvm][ipf] auto constants folding/propagation and few minor low level optimizations.
    svn path=/harmony/; revision=477525

---
+++ enhanced/drlvm/trunk/vm/jitrino/src/codegenerator/ipf/IpfCodeGenerator.cpp
@@ -0,0 +1,138 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+                                                                                                            
+/**
+ * @author Intel, Konstantin M. Anisimov, Igor V. Chebykin
+ *
+ */
+
+#include "Jitrino.h"
+#include "IpfCodeGenerator.h"
+#include "IpfCodeSelector.h"
+#include "IpfCodeLayouter.h"
+#include "IpfLiveAnalyzer.h"
+#include "IpfDce.h"
+#include "IpfRegisterAllocator.h"
+#include "IpfSpillGen.h"
+#include "IpfIrPrinter.h"
+#include "IpfEmitter.h"
+#include "IpfPrologEpilogGenerator.h"
+#include "IpfRuntimeSupport.h"
+#include "IpfCfgVerifier.h"
+//#include "IpfInstrumentator.h"
+
+namespace Jitrino {
+namespace IPF {
+
+//========================================================================================//
+// IpfCodeGenerator
+//========================================================================================//
+
+CodeGenerator::CodeGenerator(MemoryManager        &memoryManager_, 
+                             CompilationInterface &compilationInterface_) 
+    : memoryManager(memoryManager_), compilationInterface(compilationInterface_) {
+
+    cfg        = NULL;
+    methodDesc = NULL;
+}
+
+//----------------------------------------------------------------------------------------//
+
+bool CodeGenerator::genCode(MethodCodeSelector &methodCodeSelector) {
+
+    MemoryManager  mm(0x1000, "IpfCodeGenerator");
+    cfg = new(mm) Cfg(mm, compilationInterface);
+    IrPrinter irPrinter(*cfg, (char *)Log::getLogDirName());
+    methodDesc = compilationInterface.getMethodToCompile();
+
+    bool ipfLogIsOnSaved = ipfLogIsOn;
+    if (isIpfLogoutMethod(methodDesc)) {
+        ipfLogIsOn = true;
+    }
+    if(LOG_ON) {
+        const char *methodName     = methodDesc->getName();
+        const char *methodTypeName = (methodDesc->getParentType()!=NULL
+            ? methodDesc->getParentType()->getName()
+            : "");
+        const char * methodSignature = methodDesc->getSignatureString();
+
+        IPF_LOG << endl << methodTypeName << "." << methodName << methodSignature << endl;
+    }
+
+    IPF_LOG << endl << "=========== Stage: Code Selector =============================" << endl;
+    IpfMethodCodeSelector ipfMethodCodeSelector(*cfg, compilationInterface);
+    methodCodeSelector.selectCode(ipfMethodCodeSelector);
+
+    methodDesc = ipfMethodCodeSelector.getMethodDesc();
+    cfg->getOpndManager()->initCompBases((BbNode *)cfg->getEnterNode());
+    cfg->getOpndManager()->saveThisArg();
+    if(LOG_ON) irPrinter.printCfgDot("/cs.dot");
+
+    IPF_LOG << endl << "=========== Stage: Code Instrumentation ======================" << endl;
+//    Instrumentator instrumentator(*cfg);
+//    instrumentator.instrument();
+
+    IPF_LOG << endl << "=========== Stage: Code Layouter =============================" << endl;
+    CodeLayouter codeLayouter(*cfg);
+    codeLayouter.layout();
+    if(LOG_ON) irPrinter.printCfgDot("/cl.dot");
+    if(LOG_ON) irPrinter.printAsm(LOG_OUT);
+
+    IPF_LOG << endl << "=========== Stage: Liveness analyzis =========================" << endl;
+    LiveAnalyzer liveAnalyzer(*cfg);
+    liveAnalyzer.makeLiveSets(false);
+
+    IPF_LOG << endl << "=========== Stage: Dead Code Eliminator ======================" << endl;
+    Dce dce(*cfg);
+    dce.eliminate();
+    liveAnalyzer.makeLiveSets(false);
+
+    IPF_LOG << endl << "=========== Stage: Build GC Root Set =========================" << endl;
+    RuntimeSupport runtimeSupport(*cfg, compilationInterface);
+    runtimeSupport.buildRootSet();
+
+    IPF_LOG << endl << "=========== Stage: Register Allocator ========================" << endl;
+    RegisterAllocator registerAllocator(*cfg);
+    registerAllocator.allocate();
+    if(LOG_ON) irPrinter.printAsm(LOG_OUT);
+
+    IPF_LOG << endl << "=========== Stage: Prolog and Epilog Generator ===============" << endl;
+    PrologEpilogGenerator prologEpilogGenerator(*cfg);
+    prologEpilogGenerator.genPrologEpilog();
+    
+    IPF_LOG << endl << "=========== Stage: Spill Generator ===========================" << endl;
+    SpillGen spillGen(*cfg);
+    spillGen.genSpillCode();
+    if(LOG_ON) irPrinter.printAsm(LOG_OUT);
+
+    IPF_LOG << endl << "=========== Stage: Code Emitter ==============================" << endl;
+    Emitter emitter(*cfg, compilationInterface);
+    bool ret = emitter.emit();
+
+    IPF_LOG << endl << "=========== Stage: Make Runtime Info =========================" << endl;
+    runtimeSupport.makeRuntimeInfo();
+    
+    if(ret) IPF_LOG << endl << "=========== Compilation Successful ===========================" << endl;
+    else    IPF_LOG << endl << "=========== Compilation Failed ===============================" << endl;
+
+    ipfLogIsOn = ipfLogIsOnSaved;
+    return ret;
+}
+
+} // IPF
+} // Jitrino 
+
---
+++ enhanced/drlvm/trunk/vm/jitrino/src/codegenerator/ipf/include/IpfCfg.h
@@ -0,0 +1,447 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+                                                                                                            
+/**
+ * @author Intel, Konstantin M. Anisimov, Igor V. Chebykin
+ *
+ */
+
+#ifndef IPFCFG_H_
+#define IPFCFG_H_
+
+#include "Type.h"
+#include "CodeGenIntfc.h"
+#include "IpfType.h"
+#include "IpfEncoder.h"
+#include "MemoryManager.h"
+
+using namespace std;
+
+namespace Jitrino {
+namespace IPF {
+
+//========================================================================================//
+// Forward declarations
+//========================================================================================//
+
+class Opnd;
+class Inst;
+class Node;
+class BbNode;
+class Edge;
+class OpndManager;
+
+//========================================================================================//
+// Constant
+//========================================================================================//
+
+class Constant {
+public:
+                 Constant(DataKind);
+    void         setOffset(int32 offset_)    { offset = offset_; }
+    int32        getOffset()                 { return offset; }
+    void         setAddress(void *address_)  { address = address_; }
+    void         *getAddress()               { return address; }
+    void         setSize(int16 size_)        { size = size_; }
+    int16        getSize()                   { return size; }
+    DataKind     getDataKind()               { return dataKind; }
+    virtual void *getData()                  { return NULL; }
+
+protected:
+    void         *address;
+    int32        offset;
+    int16        size;
+    DataKind     dataKind;
+};
+
+//========================================================================================//
+// SwitchConstant
+//========================================================================================//
+
+class SwitchConstant : public Constant {
+public:
+               SwitchConstant(MemoryManager&);
+    void       addEdge(Edge*);
+    Edge       *getEdge(int16 choice) { return edgeList[choice]; };
+    uint16     getChoice(Edge*);
+    uint16     getChoiceCount()       { return edgeList.size(); };
+    void       *getData(void*);
+    int16      getSize();
+    virtual    ~SwitchConstant() {}
+
+protected:
+    EdgeVector edgeList;
+};
+
+//========================================================================================//
+// int64 Constants
+//========================================================================================//
+
+class Int64Constant : public Constant {
+public:
+           Int64Constant(int64 value) : Constant(DATA_I64), value(value) { setSize(sizeof(int64)); }
+    void   *getData() { return NULL; };
+    int64  getValue() { return value; };
+
+protected:
+    int64  value;
+};
+
+//========================================================================================//
+// Float Constants
+//========================================================================================//
+
+class FloatConstant : public Constant {
+public:
+           FloatConstant(float value) : Constant(DATA_S), value(value) { setSize(sizeof(float)); }
+    void   *getData() { return NULL; }
+    double getValue() { return value; };
+
+protected:
+    float  value;
+};
+
+//========================================================================================//
+// Double Constants
+//========================================================================================//
+
+class DoubleConstant : public Constant {
+public:
+           DoubleConstant(double value) : Constant(DATA_D), value(value) { setSize(sizeof(double)); }
+    void   *getData() { return NULL; }
+    double getValue() { return value; };
+
+protected:
+    double value;
+};
+
+//========================================================================================//
+// Opnd
+//========================================================================================//
+
+class Opnd : public CG_OpndHandle {
+public:
+                    Opnd(uint32, OpndKind=OPND_INVALID, DataKind=DATA_INVALID, int64=0);
+
+    uint32          getId()                         { return id; }
+    OpndKind        getOpndKind()                   { return opndKind; }
+    DataKind        getDataKind()                   { return dataKind; }
+    void            setValue(int64 value_)          { value = value_; }
+    virtual int64   getValue()                      { return value; }
+
+    bool            isReg()                         { return IpfType::isReg(opndKind); }
+    bool            isGReg()                        { return IpfType::isGReg(opndKind); }
+    bool            isFReg()                        { return IpfType::isFReg(opndKind); }
+    bool            isImm()                         { return IpfType::isImm(opndKind); }
+    bool            isFloating()                    { return IpfType::isFloating(dataKind); }
+    bool            isSigned()                      { return IpfType::isSigned(dataKind); }
+    int16           getSize()                       { return IpfType::getSize(dataKind); }
+    bool            isWritable();
+    bool            isConstant();
+    bool            isMem();
+
+    bool            isFoldableImm(int16 size) { return isFoldableImm(value, size); }
+    bool            isImm(int);
+    static bool     isFoldableImm(int64 value, int16 size);
+    
+protected:
+    uint32          id;
+    OpndKind        opndKind;
+    DataKind        dataKind;
+    int64           value;
+};
+
+//========================================================================================//
+// RegOpnd
+//========================================================================================//
+
+class RegOpnd : public Opnd {
+public:
+                RegOpnd(MemoryManager&, uint32, OpndKind, DataKind, int32=LOCATION_INVALID);
+    int64       getValue();
+    void        setLocation(int32 value_)             { value = value_; }
+    int32       getLocation()                         { return value; }
+
+    void        incSpillCost(uint32 spillCost_)       { spillCost += spillCost_; }
+    uint32      getSpillCost()                        { return spillCost; }
+    RegOpndSet  &getDepOpnds()                        { return depOpnds; }
+    void        insertDepOpnds(RegOpndSet &opnds)     { depOpnds.insert(opnds.begin(), opnds.end()); }
+    void        insertDepOpnd(RegOpnd*);
+    void        setCrossCallSite(bool crossCallSite_) { crossCallSite = crossCallSite_; }
+    bool        getCrossCallSite()                    { return crossCallSite; }
+
+    Int2OpndMap &getCoalesceCands()                   { return coalesceCands; }
+    void        addCoalesceCand(uint32 execCnt, RegOpnd *opnd) { coalesceCands.insert(make_pair(execCnt, opnd)); }
+
+    virtual     ~RegOpnd() {}
+
+protected:
+    // These fields are for register allocation algorithm
+    uint32      spillCost;          // number of opnd uses
+    RegOpndSet  depOpnds;           // opnds which can not be placed in the same reg with the opnd
+    bool        crossCallSite;      // opnd live range crosses call site
+    Int2OpndMap coalesceCands;      // 
+};
+
+//========================================================================================//
+// ConstantRef
+//========================================================================================//
+
+class ConstantRef : public Opnd {
+public:
+    ConstantRef::ConstantRef(uint32 id, Constant *constant, DataKind dataKind = DATA_CONST_REF) :
+        Opnd(id, OPND_IMM, dataKind, LOCATION_INVALID), constant(constant) {}
+
+    int64     getValue()     { return (int64)constant->getAddress(); }
+    Constant  *getConstant() { return constant; }
+
+protected:
+    Constant  *constant;
+};
+
+//========================================================================================//
+// NodeRef
+//========================================================================================//
+
+class NodeRef : public Opnd {
+public:
+    NodeRef(uint32 id, BbNode *node = NULL) 
+    : Opnd(id, OPND_IMM, DATA_NODE_REF, LOCATION_INVALID), node(node) {}
+    
+    int64    getValue();
+    void     setNode(BbNode *node_)  { node = node_; }
+    BbNode   *getNode()              { return node; }
+
+protected:
+    BbNode   *node;
+};
+
+//========================================================================================//
+// MethodRef
+//========================================================================================//
+
+class MethodRef : public Opnd {
+public:
+    MethodRef(uint32 id, MethodDesc *method = NULL) 
+    : Opnd(id, OPND_IMM, DATA_METHOD_REF, LOCATION_INVALID), method(method) {}
+    
+    int64       getValue();
+    void        setMethod(MethodDesc *method_) { method = method_; }
+    MethodDesc  *getMethod()                   { return method; }
+
+protected:
+    MethodDesc  *method;
+};
+
+//========================================================================================//
+// Inst
+//========================================================================================//
+
+class Inst {
+public:
+    Inst(MemoryManager&, InstCode, 
+         Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL);
+
+    Inst(MemoryManager&, InstCode, Completer, 
+         Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL);
+
+    Inst(MemoryManager&, InstCode, Completer, Completer, 
+         Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL);
+
+    Inst(MemoryManager&, InstCode, Completer, Completer, Completer,
+         Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL, Opnd* =NULL);
+
+    InstCode    getInstCode()                        { return instCode; }
+    void        setInstCode(InstCode instCode_)      { instCode = instCode_; }
+
+    CompVector  &getComps()                          { return compList; }
+    Completer   getComp(uint16 num)                  { return compList[num]; }
+    void        addComp(Completer comp_)             { compList.push_back(comp_); }
+    void        setComp(uint32 num, Completer comp_) { compList[num] = comp_; }
+
+    void        addOpnd(Opnd *opnd_)                 { opndList.push_back(opnd_); }
+    void        removeLastOpnd()                     { opndList.pop_back(); }
+    OpndVector  &getOpnds()                          { return opndList; }
+    void        setOpnd(uint32 num, Opnd *opnd_)     { opndList[num] = opnd_; }
+    Opnd        *getOpnd(uint32 num)                 { return opndList[num]; }
+    uint16      getNumDst()                          { return Encoder::getNumDst(instCode); }
+    uint16      getNumOpnd()                         { return Encoder::getNumOpnd(instCode); }
+
+    char        *getInstMnemonic()                   { return Encoder::getMnemonic(instCode); }
+    char        *getCompMnemonic(Completer comp)     { return Encoder::getMnemonic(comp); }
+
+    uint32      getAddr()                            { return addr; }
+    void        setAddr(uint32 addr_)                { addr = addr_; }
+    
+    bool        isBr();
+    bool        isCall();
+    bool        isRet();
+    bool        isConditionalBranch();
+    
+protected:
+    InstCode    instCode;
+    CompVector  compList;
+    OpndVector  opndList;
+    uint32      addr;       // addr == <bundle's offset in basic block> + <slot's index>
+};
+
+//========================================================================================//
+// Edge
+//========================================================================================//
+
+class Edge {
+public:
+                Edge(Node*, Node*, double, EdgeKind);
+    Node        *getSource()                { return source; }
+    Node        *getTarget()                { return target; }
+    double      getProb()                   { return prob; }
+    void        setProb(double prob_)       { prob = prob_; }
+    EdgeKind    getEdgeKind()               { return edgeKind; }
+    void        setEdgeKind(EdgeKind kind_) { edgeKind = kind_; }
+    void        remove();
+    void        insert();
+    void        changeSource(Node *source_);
+    void        changeTarget(Node *target_);
+    bool        isBackEdge();
+
+protected:
+    EdgeKind    edgeKind;
+    Node        *source;
+    Node        *target;
+    double      prob;
+};
+
+//========================================================================================//
+// ExceptionEdge
+//========================================================================================//
+
+class ExceptionEdge : public Edge {
+public:
+                ExceptionEdge(Node*, Node*, double, Type*, uint32);
+    Type        *getExceptionType()  { return exceptionType; }
+    uint32      getPriority()        { return priority; }
+
+protected:
+    Type        *exceptionType;
+    uint32      priority;
+};
+
+//========================================================================================//
+// Node
+//========================================================================================//
+
+class Node {
+public:
+                Node(MemoryManager&, uint32, uint32, NodeKind = NODE_INVALID);
+
+    void        remove();
+    void        addEdge(Edge *edge);
+    void        removeEdge(Edge *edge);
+    Edge        *getOutEdge(EdgeKind edgeKind);
+    Edge        *getOutEdge(Node *targetNode);
+    Edge        *getInEdge(EdgeKind edgeKind);
+    Edge        *getInEdge(Node *targetNode);
+    Node        *getDispatchNode();
+    void        mergeOutLiveSets(RegOpndSet &resultSet);
+
+    void        setExecCounter(uint32 execCounter_) { execCounter = execCounter_; }
+    uint32      getExecCounter()                    { return execCounter; }
+    EdgeVector  &getInEdges()                       { return inEdges; }
+    EdgeVector  &getOutEdges()                      { return outEdges; }
+    void        setNodeKind(NodeKind kind_)         { nodeKind = kind_; }
+    NodeKind    getNodeKind()                       { return nodeKind; }
+    void        setId(uint32 id_)                   { id = id_; }
+    uint32      getId()                             { return id; }
+    void        setLiveSet(RegOpndSet& liveSet_)    { liveSet = liveSet_; }
+    RegOpndSet  &getLiveSet()                       { return liveSet; }
+    void        clearLiveSet()                      { liveSet.clear(); }
+    void        setLoopHeader(Node *loopHeader_)    { loopHeader = loopHeader_; }
+    Node        *getLoopHeader()                    { return loopHeader; }
+    bool        isBb()                              { return nodeKind == NODE_BB; }
+    
+protected:
+    uint32      id;               // node unique Id
+    uint32      execCounter;      // profile info (how many times the node executes)
+    NodeKind    nodeKind;         // 
+    EdgeVector  inEdges;          // in edges list
+    EdgeVector  outEdges;         // out edges list
+    Node        *loopHeader;      // header of loop containing this node, if NULL - node is not in loop
+    RegOpndSet  liveSet;          // set of opnds alive on node enter
+};
+
+//========================================================================================//
+// BbNode
+//========================================================================================//
+
+class BbNode : public Node {
+public:
+                BbNode(MemoryManager&, uint32, uint32);
+    void        addInst(Inst *inst)                 { insts.push_back(inst); }
+    void        removeInst(Inst *inst)              { insts.erase(find(insts.begin(),insts.end(),inst)); } 
+    InstVector  &getInsts()                         { return insts; }
+    void        setAddress(uint64 address_)         { address = address_; }
+    uint64      getAddress()                        { return address; }
+    void        setLayoutSucc(BbNode *layoutSucc_)  { layoutSucc = layoutSucc_; }
+    BbNode      *getLayoutSucc()                    { return layoutSucc; }
+    uint64      getInstAddr(Inst *inst)             { return ((uint64)address + inst->getAddr()); }
+
+protected:
+    InstVector  insts;
+    BbNode      *layoutSucc;
+    uint64      address;
+};
+
+//========================================================================================//
+// Cfg
+//========================================================================================//
+
+class Cfg {
+public:
+                         Cfg(MemoryManager &mm, CompilationInterface &compilationInterface);
+    NodeVector           &search(SearchKind searchKind);
+    
+    MemoryManager        &getMM()                       { return mm; }
+    CompilationInterface &getCompilationInterface()     { return compilationInterface; }
+    uint16               getNextNodeId()                { return maxNodeId++; }
+    uint16               getMaxNodeId()                 { return maxNodeId; }
+    void                 setEnterNode(Node *enterNode_) { enterNode = enterNode_; }
+    void                 setExitNode(Node *exitNode_)   { exitNode = exitNode_; }
+    Node                 *getEnterNode()                { return enterNode; }
+    Node                 *getExitNode()                 { return exitNode; }
+    OpndManager          *getOpndManager()              { return opndManager; }
+    MethodDesc           *getMethodDesc()               { return compilationInterface.getMethodToCompile(); }
+
+protected:
+    void                 makePostOrdered(Node *node, NodeSet &visitedNodes);
+    void                 makeDirectOrdered(Node *node, NodeSet &visitedNodesd);
+    void                 makeLayoutOrdered();
+
+    MemoryManager        &mm;
+    CompilationInterface &compilationInterface;
+
+    uint16               maxNodeId;
+    OpndManager          *opndManager;
+    Node                 *enterNode;
+    Node                 *exitNode;
+    NodeVector           searchResult;
+    SearchKind           lastSearchKind;
+};
+
+} // IPF
+} // Jitrino
+
+#endif /*IPFCFG_H_*/
