1be2423f12c264c64683035f6233bbe80afabf59
Gregory Shimansky <apache@apache.org>
2006-11-23 19:31:04 +0000

    Applied HARMONY-2285 [drlvm] several NIO tests crash on DRLVM
    Tests passed on windows 2003 server. Since no other platforms are affected
    no more testing was done
    
    
    svn path=/harmony/; revision=478650

---
+++ enhanced/drlvm/trunk/vm/vmcore/src/util/win/ia32/nt_exception_filter.cpp
@@ -0,0 +1,434 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+/** 
+ * @author Intel, Evgueni Brevnov
+ * @version $Revision: 1.1.2.1.4.4 $
+ */  
+
+#include "cxxlog.h"
+#include "method_lookup.h"
+#include "Environment.h"
+#include "exceptions.h"
+#include "exceptions_jit.h"
+#include "interpreter_exports.h"
+#include "stack_dump.h"
+#include "jvmti_break_intf.h"
+
+// Windows specific
+#include <string>
+#include <excpt.h>
+
+#if INSTRUMENTATION_BYTE == INSTRUMENTATION_BYTE_INT3
+#define JVMTI_EXCEPTION_STATUS STATUS_BREAKPOINT
+#elif INSTRUMENTATION_BYTE == INSTRUMENTATION_BYTE_HLT || INSTRUMENTATION_BYTE == INSTRUMENTATION_BYTE_CLI
+#define JVMTI_EXCEPTION_STATUS STATUS_PRIVILEGED_INSTRUCTION
+#else
+#error Unknown value of INSTRUMENTATION_BYTE
+#endif
+
+void nt_to_vm_context(PCONTEXT context, Registers* regs)
+{
+    regs->eax = context->Eax;
+    regs->ecx = context->Ecx;
+    regs->edx = context->Edx;
+    regs->edi = context->Edi;
+    regs->esi = context->Esi;
+    regs->ebx = context->Ebx;
+    regs->ebp = context->Ebp;
+    regs->eip = context->Eip;
+    regs->esp = context->Esp;
+    regs->eflags = context->EFlags;
+}
+
+void vm_to_nt_context(Registers* regs, PCONTEXT context)
+{
+    context->Esp = regs->esp;
+    context->Eip = regs->eip;
+    context->Ebp = regs->ebp;
+    context->Ebx = regs->ebx;
+    context->Esi = regs->esi;
+    context->Edi = regs->edi;
+    context->Eax = regs->eax;
+    context->Ecx = regs->ecx;
+    context->Edx = regs->edx;
+    context->EFlags = regs->eflags;
+}
+
+static void print_state(LPEXCEPTION_POINTERS nt_exception, const char *msg)
+{
+    fprintf(stderr, "...VM Crashed!\n");
+    if (msg != 0) 
+    {
+        fprintf(stderr, "Windows reported exception: %s\n", msg);
+    }
+    else 
+    {
+        fprintf(stderr, "Windows reported exception: 0x%x\n", nt_exception->ExceptionRecord->ExceptionCode);
+    }
+
+    fprintf(stderr, "Registers:\n");
+    fprintf(stderr, "    EAX: 0x%08x, EBX: 0x%08x, ECX: 0x%08x, EDX=0x%08x\n",
+            nt_exception->ContextRecord->Eax,
+            nt_exception->ContextRecord->Ebx,
+            nt_exception->ContextRecord->Ecx,
+            nt_exception->ContextRecord->Edx);
+    fprintf(stderr, "    ESI: 0x%08x, EDI: 0x%08x, ESP: 0x%08x, EBP=0x%08x\n",
+            nt_exception->ContextRecord->Esi,
+            nt_exception->ContextRecord->Edi,
+            nt_exception->ContextRecord->Esp,
+            nt_exception->ContextRecord->Ebp);
+    fprintf(stderr, "    EIP: 0x%08x\n", nt_exception->ContextRecord->Eip);
+}
+
+
+static void print_callstack(LPEXCEPTION_POINTERS nt_exception) {
+    PCONTEXT context = nt_exception->ContextRecord;
+    Registers regs;
+    nt_to_vm_context(context, &regs);
+    st_print_stack(&regs);
+    fflush(stderr);
+}
+
+/*
+ * Information about stack
+ */
+inline void* find_stack_addr() {
+    void* stack_addr;
+    size_t reg_size;
+    MEMORY_BASIC_INFORMATION memory_information;
+
+    VirtualQuery(&memory_information, &memory_information, sizeof(memory_information));
+    reg_size = memory_information.RegionSize;
+    stack_addr =((char*) memory_information.BaseAddress) + reg_size;
+
+    return stack_addr;
+}
+
+inline size_t find_stack_size() {
+   void* stack_addr;
+    size_t stack_size;
+    size_t reg_size;
+    MEMORY_BASIC_INFORMATION memory_information;
+
+    VirtualQuery(&memory_information, &memory_information, sizeof(memory_information));
+    reg_size = memory_information.RegionSize;
+    stack_addr = ((char*) memory_information.BaseAddress) + reg_size;
+    stack_size = ((char*) stack_addr) - ((char*) memory_information.AllocationBase);
+
+    return stack_size;
+}
+
+inline size_t find_guard_page_size() {
+    size_t  guard_size;
+    SYSTEM_INFO system_info;
+
+    GetSystemInfo(&system_info);
+    guard_size = system_info.dwPageSize;
+
+    return guard_size;
+}
+
+inline size_t find_guard_stack_size() {
+    // guaerded stack size on windows can be equals one page size only :(
+    return find_guard_page_size();
+}
+
+static size_t common_stack_size;
+static size_t common_guard_stack_size;
+static size_t common_guard_page_size;
+
+inline void* get_stack_addr() {
+    return p_TLS_vmthread->stack_addr;
+}
+
+inline size_t get_stack_size() {
+    return common_stack_size;
+}
+
+inline size_t get_guard_stack_size() {
+    return common_guard_stack_size;
+}
+
+inline size_t get_guard_page_size() {
+    return common_guard_page_size;
+}
+
+
+void init_stack_info() {
+    p_TLS_vmthread->stack_addr = find_stack_addr();
+    common_stack_size = find_stack_size();
+    common_guard_stack_size = find_guard_stack_size();
+    common_guard_page_size =find_guard_page_size();
+}
+
+void set_guard_stack() {
+    void* stack_addr = get_stack_addr();
+    size_t stack_size = get_stack_size();
+    size_t page_size = get_guard_page_size();
+
+    if (!VirtualFree((char*)stack_addr - stack_size + page_size,
+        page_size, MEM_DECOMMIT)) {
+        // should be successful always
+        assert(0);
+    }
+
+    DWORD oldProtect;
+
+    if (!VirtualProtect((char*)stack_addr - stack_size + page_size + page_size,
+        page_size, PAGE_GUARD | PAGE_READWRITE, &oldProtect)) {
+        // should be successful always
+        assert(0);
+    }
+
+    p_TLS_vmthread->restore_guard_page = false;
+}
+
+size_t get_available_stack_size() {
+    char* stack_adrr = (char*) get_stack_addr();
+    size_t used_stack_size = ((size_t)stack_adrr) - ((size_t)(&stack_adrr));
+    size_t available_stack_size =
+            get_stack_size() - used_stack_size
+            - get_guard_page_size() - get_guard_stack_size();
+    return available_stack_size;
+}
+size_t get_default_stack_size() {
+    size_t default_stack_size = get_stack_size();
+    return default_stack_size;
+}
+bool check_available_stack_size(size_t required_size) {
+    if (get_available_stack_size() < required_size) {
+        Global_Env *env = VM_Global_State::loader_env;
+        exn_raise_by_class(env->java_lang_StackOverflowError_Class);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// exception catch callback to restore stack after Stack Overflow Error
+static void __cdecl exception_catch_callback_wrapper(){
+    exception_catch_callback();
+}
+
+// exception catch support for JVMTI
+static void __cdecl jvmti_exception_catch_callback_wrapper(Registers regs){
+    jvmti_exception_catch_callback(&regs);
+}
+
+static void __declspec(naked) asm_exception_catch_callback() {
+    __asm {
+        push ebp
+        mov ebp, esp
+        push eax
+        push ebx
+        push ecx
+        push edx
+        call exception_catch_callback_wrapper
+        pop edx
+        pop ecx
+        pop ebx
+        pop eax
+        leave
+        ret
+    }
+}
+
+void __declspec(naked) asm_jvmti_exception_catch_callback() {
+    __asm {
+        push ebp
+        mov ebp, esp
+        add esp, -36
+        mov [ebp-36], eax
+        mov [ebp-32], ebx
+        mov [ebp-28], ecx
+        mov [ebp-24], edx
+        mov eax, esp
+        mov ebx, [ebp]
+        mov ecx, [ebp+4]
+        add eax, 44
+        mov [ebp-20], edi
+        mov [ebp-16], esi
+        mov [ebp-12], ebx
+        mov [ebp-8], eax
+        mov [ebp-4], ecx
+        call jvmti_exception_catch_callback_wrapper
+        mov eax, [ebp-36]
+        mov ebx, [ebp-32]
+        mov ecx, [ebp-28]
+        mov edx, [ebp-24]
+        add esp, 36
+        leave
+        ret
+    }
+}
+
+static LONG NTAPI vectored_exception_handler_internal(LPEXCEPTION_POINTERS nt_exception);
+
+LONG __declspec(naked) NTAPI vectored_exception_handler(LPEXCEPTION_POINTERS nt_exception)
+{
+    __asm {
+    push    ebp
+    mov     ebp,esp
+    pushfd
+    cld
+    mov     eax, [ebp + 8]
+    push    eax
+    call    vectored_exception_handler_internal
+    popfd
+    pop     ebp
+    ret     4    
+    }
+}
+
+static LONG NTAPI vectored_exception_handler_internal(LPEXCEPTION_POINTERS nt_exception)
+{
+    DWORD code = nt_exception->ExceptionRecord->ExceptionCode;
+    
+    bool run_default_handler = true;
+    PCONTEXT context = nt_exception->ContextRecord;
+    
+    TRACE2("signals", "VEH received an exception: code = 0x" <<
+        ((void*)nt_exception->ExceptionRecord->ExceptionCode) <<
+        " location IP = 0x" << ((void*)context->Eip));
+    
+    // this filter catches _all_ hardware exceptions including those caused by
+    // VM internal code.  To elimate confusion over what caused the
+    // exception, we first make sure the exception was thrown inside a Java
+    // method else crash handler or default handler is executed, this means that
+    // it was thrown by VM C/C++ code.
+    if (((code == STATUS_ACCESS_VIOLATION ||
+        code == STATUS_INTEGER_DIVIDE_BY_ZERO ||
+        code == STATUS_STACK_OVERFLOW) &&
+        vm_identify_eip((void *)context->Eip) == VM_TYPE_JAVA) ||
+        code == JVMTI_EXCEPTION_STATUS) {
+            run_default_handler = false;
+    } else if (code == STATUS_STACK_OVERFLOW) {
+        if (is_unwindable()) {
+            if (hythread_is_suspend_enabled()) {
+                tmn_suspend_disable();
+            }
+            run_default_handler = false;
+        } else {
+                Global_Env *env = VM_Global_State::loader_env;
+                exn_raise_by_class(env->java_lang_StackOverflowError_Class);
+            p_TLS_vmthread->restore_guard_page = true;
+            return EXCEPTION_CONTINUE_EXECUTION;
+        }
+    }
+
+    if (run_default_handler) {
+#ifndef NDEBUG
+        if (vm_get_boolean_property_value_with_default("vm.assert_dialog")) {
+
+            if (IS_ERROR(code)) {
+                 if (UnhandledExceptionFilter(nt_exception) == EXCEPTION_CONTINUE_SEARCH)
+                     DebugBreak();
+            }
+        }
+#endif
+        return EXCEPTION_CONTINUE_SEARCH;
+    }
+
+    // since we are now sure HWE occured in java code, gc should also have been disabled
+
+    // gregory - this is not true since for debugging we may use int3
+    // in VM code which produces BREAKPOINT exception. JVMTI has
+    // assertions for breakpoints which it has set in Java inside of
+    // breakpoint handling function. Otherwise this assert should not
+    // fail in case _CrtDbgBreak() was added somewhere in VM.
+    assert(!hythread_is_suspend_enabled() || code == JVMTI_EXCEPTION_STATUS);
+    
+    Global_Env *env = VM_Global_State::loader_env;
+    Class *exc_clss = 0;
+
+    switch(nt_exception->ExceptionRecord->ExceptionCode) 
+    {
+    case STATUS_STACK_OVERFLOW:
+        // stack overflow exception -- see ...\vc\include\winnt.h
+        {
+            TRACE2("signals", "StackOverflowError detected at "
+                << (void *) context->Eip << " on the stack at "
+                << (void *) context->Esp);
+            // Lazy exception object creation
+            exc_clss = env->java_lang_StackOverflowError_Class;
+            p_TLS_vmthread->restore_guard_page = true;
+        }
+        break;
+    case STATUS_ACCESS_VIOLATION:
+        // null pointer exception -- see ...\vc\include\winnt.h
+        {
+            TRACE2("signals", "NullPointerException detected at " 
+                << (void *) context->Eip);
+            // Lazy exception object creation
+            exc_clss = env->java_lang_NullPointerException_Class;
+        }
+        break;
+
+    case STATUS_INTEGER_DIVIDE_BY_ZERO:
+        // divide by zero exception  -- see ...\vc\include\winnt.h
+        {
+            TRACE2("signals", "ArithmeticException detected at "
+                << (void *) context->Eip);
+            // Lazy exception object creation
+            exc_clss = env->java_lang_ArithmeticException_Class;
+        }
+        break;
+    case JVMTI_EXCEPTION_STATUS:
+        // JVMTI breakpoint in JITted code
+        {
+            Registers regs;
+            nt_to_vm_context(context, &regs);
+            TRACE2("signals", "JVMTI breakpoint detected at " <<
+                (void *)regs.eip);
+            bool handled = jvmti_jit_breakpoint_handler(&regs);
+            if (handled)
+            {
+                vm_to_nt_context(&regs, context);
+                return EXCEPTION_CONTINUE_EXECUTION;
+            }
+            else
+                return EXCEPTION_CONTINUE_SEARCH;
+        }
+    default: assert(false);
+    }
+
+    Registers regs;
+
+    nt_to_vm_context(context, &regs);
+
+    uint32 exception_esp = regs.esp;
+    DebugUtilsTI* ti = VM_Global_State::loader_env->TI;
+
+    // TODO: We already checked that above. Is it possible to reuse the result?
+    bool java_code = (vm_identify_eip((void *)regs.eip) == VM_TYPE_JAVA);
+    exn_athrow_regs(&regs, exc_clss, java_code);
+
+    assert(exception_esp <= regs.esp);
+    if (ti->get_global_capability(DebugUtilsTI::TI_GC_ENABLE_EXCEPTION_EVENT)) {
+        regs.esp = regs.esp - 4;
+        *((uint32*) regs.esp) = regs.eip;
+        regs.eip = ((uint32)asm_jvmti_exception_catch_callback);
+    } else if (p_TLS_vmthread->restore_guard_page) {
+        regs.esp = regs.esp - 4;
+        *((uint32*) regs.esp) = regs.eip;
+        regs.eip = ((uint32)asm_exception_catch_callback);
+    }
+
+    vm_to_nt_context(&regs, context);
+
+    return EXCEPTION_CONTINUE_EXECUTION;
+} //vectored_exception_handler
