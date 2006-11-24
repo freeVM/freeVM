d22d9e35e9d4bdd3bd7138a5c15d5ee9d9ed2c6a
Mikhail Loenko <apache@apache.org>
2006-11-24 06:26:52 +0000

    mark not implemented methods to get more real JAPI results
    svn path=/harmony/; revision=478778

---
+++ enhanced/classlib/trunk/modules/sound/src/main/java/javax/sound/sampled/Line.java
@@ -0,0 +1,62 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package javax.sound.sampled;
+
+import org.apache.harmony.luni.util.NotImplementedException;
+
+public interface Line {
+
+    class Info {
+        private Class <?> lineClass;
+        
+        public Info(Class <?> lineClass) {
+            this.lineClass = lineClass;
+        }
+
+        public Class <?> getLineClass( ){
+            return lineClass;
+        }
+
+        public boolean matches(Line.Info info) throws NotImplementedException {
+            throw new NotImplementedException("not yet implemented");
+        }
+        
+        @Override
+        public String toString() throws NotImplementedException {
+            throw new NotImplementedException("not yet implemented");
+        }
+    }
+
+    void addLineListener(LineListener listener);
+
+    void close();
+
+    Control getControl(Control.Type control);
+
+    Control[] getControls();
+
+    Line.Info getLineInfo();
+
+    boolean isControlSupported(Control.Type control);
+
+    boolean isOpen();
+
+    void open() throws LineUnavailableException;
+
+    void removeLineListener(LineListener listener);
+}
