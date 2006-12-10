0b38bfc00fb5b4b22a51b3695bf1d991e6574a48
Geir Magnusson Jr <apache@apache.org>
2006-12-10 15:10:33 +0000

    HARMONY-2543
    Additional commit of the changes to the tests.  Kinda critical
    
    
    svn path=/harmony/; revision=485192

---
+++ enhanced/drlvm/trunk/vm/tests/smoke/stress/Stack.java
@@ -0,0 +1,47 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+/** 
+ * @author Alexei Fedotov
+ * @version $Revision: 1.5.20.3 $
+ */  
+package stress;
+
+public class Stack {
+
+    static final int depth = 7000; // the external java crashes if depth = 200000
+
+    synchronized boolean test(int i) {
+        System.out.println("" + i);
+
+        if (i < depth && test(++i)) {
+            return true;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        boolean pass = false;
+
+        try {
+            Stack test = new Stack();
+            pass = test.test(0);
+        } catch (Throwable e) {
+            System.out.println("Got exception: " + e.toString());            
+        }
+        System.out.println("Stack test " + (pass ? "passed" : "failed"));
+    }
+}
