f8fed1b2e6e9293ba3aa1947b5c35143c8b26228
Gregory Shimansky <apache@apache.org>
2007-07-23 14:37:24 +0000

    Applied patch from HARMONY-3508 [jdktools][jpda] JDWP test org.apache.harmony.jpda.tests.jdwp.VirtualMachine.HoldEventsTest intermittently fails
    svn path=/harmony/enhanced/jdktools/trunk/; revision=558745

--- modules/jpda/test/common/unit/org/apache/harmony/jpda/tests/jdwp/VirtualMachine/HoldEventsTest.java
+++ modules/jpda/test/common/unit/org/apache/harmony/jpda/tests/jdwp/VirtualMachine/HoldEventsTest.java
@@ -27,9 +27,11 @@ package org.apache.harmony.jpda.tests.jdwp.VirtualMachine;
 
 import org.apache.harmony.jpda.tests.framework.TestErrorException;
 import org.apache.harmony.jpda.tests.framework.jdwp.CommandPacket;
+import org.apache.harmony.jpda.tests.framework.jdwp.EventPacket;
 import org.apache.harmony.jpda.tests.framework.jdwp.JDWPCommands;
 import org.apache.harmony.jpda.tests.framework.jdwp.JDWPConstants;
 import org.apache.harmony.jpda.tests.framework.jdwp.ReplyPacket;
+import org.apache.harmony.jpda.tests.framework.jdwp.exceptions.TimeoutException;
 import org.apache.harmony.jpda.tests.jdwp.share.JDWPSyncTestCase;
 import org.apache.harmony.jpda.tests.share.JPDADebuggeeSynchronizer;
 
@@ -37,8 +39,6 @@ import org.apache.harmony.jpda.tests.share.JPDADebuggeeSynchronizer;
  * JDWP Unit test for VirtualMachine.HoldEvents command.
  */
 public class HoldEventsTest extends JDWPSyncTestCase {
-
-    CommandPacket event = null;
 
     protected String getDebuggeeClassName() {
         return "org.apache.harmony.jpda.tests.jdwp.VirtualMachine.HoldEventsDebuggee";
@@ -50,8 +50,10 @@ public class HoldEventsTest extends JDWPSyncTestCase {
      * <BR> Then the test sends request for TESTED_THREAD and
      * performs VirtualMachine.HoldEvents command.
      * Next, the test waits for debuggee to start the 'TESTED_THREAD'
-     * thread and checks that no any events (including requested TESTED_THREAD event)
-     * are sent to test.
+     * thread and checks that no any events (including requested THREAD_START event)
+     * are received during default timeout.
+     * Then the test sends VirtualMachine.ReleaseEvents command 
+     * and checks that expected THREAD_START event is received.
      */
     public void testHoldEvents001() {
         synchronizer.receiveMessage(JPDADebuggeeSynchronizer.SGNL_READY);
@@ -70,19 +72,14 @@ public class HoldEventsTest extends JDWPSyncTestCase {
         logWriter.println("allow to start thread");
         synchronizer.sendMessage(JPDADebuggeeSynchronizer.SGNL_CONTINUE);
 
-        EventReceiver thread = new EventReceiver(); 
-        thread.start();
         try {
-            thread.join(settings.getTimeout());
-        } catch (InterruptedException e) {
-            
-        }
-
-        if (event == null) {
+            EventPacket event = debuggeeWrapper.vmMirror.receiveEvent(settings.getTimeout());
+            logWriter.printError("unexpected event received: " + event);
+            fail("unexpected event received");
+        } catch (TimeoutException e) {
             logWriter.println("no events were received");
-        } else {
-            logWriter.printError("unexpected event");
-            fail("unexpected event");
+        } catch (Exception e) {
+            throw new TestErrorException(e);
         }
 
         logWriter.println("send ReleaseEvents");
@@ -91,19 +88,11 @@ public class HoldEventsTest extends JDWPSyncTestCase {
                 JDWPCommands.VirtualMachineCommandSet.ReleaseEventsCommand);
         debuggeeWrapper.vmMirror.performCommand(packet);
        
-        event = debuggeeWrapper.vmMirror.receiveCertainEvent(JDWPConstants.EventKind.THREAD_START);
+        EventPacket event = debuggeeWrapper.vmMirror.receiveCertainEvent(JDWPConstants.EventKind.THREAD_START);
+        logWriter.println("expected event received: " + event);
         debuggeeWrapper.vmMirror.resume();
     }
 
-    class EventReceiver extends Thread {
-        public void run() {
-            try {
-                event = debuggeeWrapper.vmMirror.receiveCertainEvent(JDWPConstants.EventKind.THREAD_START);
-            } catch (TestErrorException e) {
-                logWriter.println(e.toString());
-            }
-        }
-    }
     public static void main(String[] args) {
         junit.textui.TestRunner.run(HoldEventsTest.class);
     }
