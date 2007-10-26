1fc6da4029fe62f9796f23588c05951dfaa02d1d
Oliver Deakin <apache@apache.org>
2007-10-26 11:58:40 +0000

    Let JUnit deal with unexpected Exceptions rather than catching them ourselves.
    svn path=/harmony/; revision=588615

--- enhanced/classlib/trunk/modules/awt/src/test/api/java/common/java/awt/CardLayoutRTest.java
+++ enhanced/classlib/trunk/modules/awt/src/test/api/java/common/java/awt/CardLayoutRTest.java
@@ -22,10 +22,10 @@ package java.awt;
 
 import junit.framework.TestCase;
 
-public class BorderLayoutRTest extends TestCase {
+public class CardLayoutRTest extends TestCase {
     Container emptyContainer;
     Dimension defSize;
-    BorderLayout layout;
+    CardLayout layout;
     private Dimension maxSize;
 
     @Override
@@ -34,8 +34,18 @@ public class BorderLayoutRTest extends TestCase {
         emptyContainer = new Container();
         defSize = new Dimension();
         maxSize = new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE);
-        layout = new BorderLayout();
+        layout = new CardLayout();
     }
+    
+    public void testFirst() {        
+        try {
+            layout.first(emptyContainer);
+        } catch (IllegalArgumentException iae) {
+            return;            
+        }
+        fail("Expected IllegalArgumentException");
+    }
+
     
     public final void testGetLayoutAlignmentX1() {
         layout.getLayoutAlignmentX(null);
@@ -53,12 +63,18 @@ public class BorderLayoutRTest extends TestCase {
         layout.getLayoutAlignmentY(emptyContainer);
     }
     
-    public final void testInvalidateLayout1() {
-        layout.invalidateLayout(null);
+    public void testLast() {        
+        try {
+            layout.last(emptyContainer);
+        } catch (IllegalArgumentException iae) {
+            return;            
+        }
+        fail("Expected IllegalArgumentException");
     }
     
-    public final void testInvalidateLayout2() {        
-        layout.invalidateLayout(emptyContainer);
+    public void testLayoutContainer() {       
+        layout.layoutContainer(emptyContainer);
+        assertTrue(true);
     }
     
     public final void testMaximumLayoutSize1() {
@@ -73,54 +89,31 @@ public class BorderLayoutRTest extends TestCase {
     public final void testMinimumLayoutSize() {
         assertEquals(defSize, layout.minimumLayoutSize(emptyContainer));
     }
-
+    
+    public void testNext() {        
+        try {
+            layout.next(emptyContainer);
+        } catch (IllegalArgumentException iae) {
+            return;            
+        }
+        fail("Expected IllegalArgumentException");
+    }
+    
     public final void testPreferredLayoutSize() {
         assertEquals(defSize, layout.preferredLayoutSize(emptyContainer));
     }   
     
-    /**
-     * Regression for HARMONY-4085
-     */
-    public final void testHarmony4085() {
-        final Frame f = new Frame();
-        final boolean[] isInvoked = new boolean[4];
-
-        f.add(new Component() {
-            @Override
-            public void setSize(int width, int height) {
-                isInvoked[0] = true;
-                super.setSize(width, height);
-            }
-        }, BorderLayout.EAST);
-        f.add(new Component() {
-            @Override
-            public void setSize(int width, int height) {
-                isInvoked[1] = true;
-                super.setSize(width, height);
-            }
-        }, BorderLayout.WEST);
-        f.add(new Component() {
-            @Override
-            public void setSize(int width, int height) {
-                isInvoked[2] = true;
-                super.setSize(width, height);
-            }
-        }, BorderLayout.NORTH);
-        f.add(new Component() {
-            @Override
-            public void setSize(int width, int height) {
-                isInvoked[3] = true;
-                super.setSize(width, height);
-            }
-        }, BorderLayout.SOUTH);
-
-        f.setSize(100, 100);
-        f.setVisible(true);
-        f.dispose();
-
-        assertTrue(isInvoked[0]);
-        assertTrue(isInvoked[1]);
-        assertTrue(isInvoked[2]);
-        assertTrue(isInvoked[3]);
+    public void testPrevious() {        
+        try {
+            layout.previous(emptyContainer);
+        } catch (IllegalArgumentException iae) {
+            return;            
+        }
+        fail("Expected IllegalArgumentException");
     }
+    
+    public void testRemoveLayoutComponent() {       
+        layout.removeLayoutComponent(emptyContainer);
+    }
+    
 }
