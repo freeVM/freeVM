650d2d509b0a09a2f33ee9c8d847f94d89320cfb
Stepan Mishura <apache@apache.org>
2007-11-08 04:31:37 +0000

    Apply modified patch to fix report location from HARMONY-5082: ([buildtest][eut] fix report location and EFL)
    svn path=/harmony/enhanced/buildtest/branches/2.0/; revision=593008

--- tests/eut/summary-reporter/src/org/apache/harmony/eut/reporter/EUTTXTReportEmitter.java
+++ tests/eut/summary-reporter/src/org/apache/harmony/eut/reporter/EUTTXTReportEmitter.java
@@ -25,6 +25,12 @@ import java.io.PrintWriter;
  */
 final class EUTTXTReportEmitter {
 
+    /** 
+     * Holds the maximum number of unexpected errors/failure to be included to
+     * report.
+     */
+    private static int MAX_ERROR_FAILURE_NUMBER = 10;
+
     /** Keeps the output stream to write the report.txt data into. */
     private static PrintWriter out;
 
@@ -104,7 +110,9 @@ final class EUTTXTReportEmitter {
         } else {
             out.println("Unexpected failures : " + issuesNum);
         }
+        int reported_issues_num = 0;
 
+EMIT_ERROR_FAILURE_RESULTS:
         for (int i = 0; i < EUTReporter.suiteList.size(); i++) {
             EUTSuiteInfo si = EUTReporter.suiteList.get(i);
 
@@ -131,6 +139,20 @@ final class EUTTXTReportEmitter {
                 out.println(ti.testClass + '.' + ti.testName);
                 out.println(ti.testIssueMessage);
                 out.println(ti.testIssueContent.toString().trim());
+                reported_issues_num++;
+
+                /*
+                 * The resulted report must not be too big, so one needs to
+                 * limit a number of reported issues...
+                 */
+                if (reported_issues_num == MAX_ERROR_FAILURE_NUMBER) {
+                    if (reported_issues_num !=
+                            si.unexpectedErrorFailureTests.size() - 1) {
+                        out.println();
+                        out.println("...Skipped the rest of reports...");
+                    }
+                    break EMIT_ERROR_FAILURE_RESULTS;
+                }
             }
         }
     }
