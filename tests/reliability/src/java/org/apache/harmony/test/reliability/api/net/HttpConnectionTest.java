6fb4d955000f7277e255ae4385e817638c78f510
Stepan Mishura <apache@apache.org>
2007-12-11 11:26:48 +0000

    Apply patch from HARMONY-5266: ([buildtest][reliability] HttpConnectionTest fails when www.apache.org is not accessible)
    svn path=/harmony/enhanced/buildtest/branches/2.0/; revision=603210

--- tests/reliability/src/java/org/apache/harmony/test/reliability/api/net/HttpConnectionTest.java
+++ tests/reliability/src/java/org/apache/harmony/test/reliability/api/net/HttpConnectionTest.java
@@ -24,12 +24,16 @@ package org.apache.harmony.test.reliability.api.net;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.Proxy;
 import java.net.URL;
+import java.net.Socket;
+import java.net.ServerSocket;
 import java.util.regex.Pattern;
 
 import org.apache.harmony.test.reliability.share.Test;
@@ -37,7 +41,7 @@ import org.apache.harmony.test.reliability.share.Test;
 /*
  * Goal: Test HttpURLConnection functionality
  *
- *  passed parameters:
+ *  passed optional parameters:
  *  parameter[0] - urls for web pages you want to get, all urls are to be listed consequently
  *  via ";" with no blanks or tabs
  *  E.g http://harmony.apache.org/;http://issues.apache.org/jira/browse/HARMONY
@@ -49,13 +53,14 @@ import org.apache.harmony.test.reliability.share.Test;
  *   
  *  No crash, hang or fail is excpected.
  *  
- *  Note: All input parameters are to be set properly as test could fail due to wrong 
- *  set proxy or Url address.
+ *  If no parameters are set then internal stab http server is started for testing.
  *  */
 
 public class HttpConnectionTest extends Test{
+    static volatile boolean failed = false;
     String[] testedUrlStrings = null;
     Proxy prx = null;
+    HttpServerStub sPort = null;
 
     public static void main(String[] params){
         System.exit(new HttpConnectionTest().test(params));
@@ -63,8 +68,16 @@ public class HttpConnectionTest extends Test{
     
     public int test(String[] params){
         parseParams(params);
+
+
         if (testedUrlStrings == null){
-            return fail("Urls to test are not set");
+            sPort = new HttpServerStub();
+            if (!sPort.isAlive())
+            {
+                return fail("Failed to start server");
+            }
+            testedUrlStrings = new String[1];
+            testedUrlStrings[0] = "http://localhost:" + sPort.port + "/";
         }
 
         for (int i=0; i<testedUrlStrings.length;i++){
@@ -79,7 +92,11 @@ public class HttpConnectionTest extends Test{
                 return fail("FAILED");
             }
         }
-        
+
+        if (failed == true)
+        {
+            return fail("FAILED");
+        }
         return pass("OK");
     }
     
@@ -115,11 +132,12 @@ public class HttpConnectionTest extends Test{
             while((buf = br.readLine()) != null){
                 resultedString += buf;
             }
-            //System.out.println(resultedString);
+            // System.out.println("resultedString:" + resultedString);
             hur.disconnect();
             
         } catch (IOException e) {
             log.add("Failed to connect to " + hur.getURL() + " : " + e.getMessage());
+            e.printStackTrace();
             return false;
         }
         
@@ -138,3 +156,90 @@ public class HttpConnectionTest extends Test{
         
     }
 }
+
+class HttpServerStub extends Thread
+{
+    int port = 0;
+    ServerSocket ss = null;
+    boolean started = false;
+    final static int sizeOfMessage = 1000;
+    String content = null;
+    HttpServerStub()
+    {
+        try
+        {
+            ss = new ServerSocket(0);
+        }
+        catch (IOException e1)
+        {
+            return;
+        }
+        port = ss.getLocalPort();
+        start();
+        started = true;
+    }
+    public void close()
+    {
+        try {
+            if (started)
+            {
+                started = false;
+                ss.close(); 
+            }
+        }
+        catch (IOException ioe)
+        {
+        }
+    }
+    public void run()
+    {
+        try
+        {
+            Socket socket = ss.accept();
+            try
+            {
+                InputStream in = socket.getInputStream();
+                OutputStream out = socket.getOutputStream();
+
+                byte[] buf = new byte[sizeOfMessage];
+                int red = in.read(buf, 0, sizeOfMessage);
+                // The content is so big because otherwise the test is too fast and
+                // could exhaust all operating system sockets causing the failure 
+                content = "Content-Type:   text/html;charset=iso-8859-1\n\n<html>\n<body>\n";
+                for (int l = 0; l < 2000; l++)
+                {
+                    content += "<p>Hello, Word!\n";
+                }
+                content += "</body>\n</html>\n";
+                out.write(content.getBytes());
+            }
+            catch (IOException e)
+            {
+                HttpConnectionTest.log.add("Server thread " + getId() + " could not get streams: " + e.getMessage());
+                e.printStackTrace();
+                HttpConnectionTest.failed = true;
+                return;
+            }
+            socket.close();
+        }
+        catch (IOException e)
+        {
+            HttpConnectionTest.log.add("Server thread " + getId() + " could not accept request: " + e.getMessage());
+            e.printStackTrace();
+            HttpConnectionTest.failed = true;
+            return;
+        }
+
+        try
+        {
+            ss.close();
+        }
+        catch (IOException e)
+        {
+            HttpConnectionTest.log.add("Failed to close ServerSocket on port " + port);
+            HttpConnectionTest.failed = true;
+            return;
+        }
+    }
+}
+
