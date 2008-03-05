freeVM/freeVM

commit 08527b749231df492ef91d5f0d18f1eb2f324e16

Applied patches 0011 and 0012 from HARMONY-5504 [drlvm][port] Restructure DRLVM's sources to extract most of platform dependent code into portlib
Patches move signals and exceptions handlers to port library and implement
universal transfer control code to be used in crash handler. Singlas and
exceptions handling code in VM was adopted to use this functionality.


svn path=/harmony/; revision=633414

enhanced/drlvm/trunk/vm/port/src/thread/linux/thread_asm_em64t.s
@@ -41,11 +41,11 @@
 // uint32 eflags;; 88h
 // };
 //
-// void transfer_to_regs(Registers* regs)
+// void port_transfer_to_regs(Registers* regs)
 
-.globl transfer_to_regs
-	.type	transfer_to_regs, @function
-transfer_to_regs:
+.globl port_transfer_to_regs
+	.type	port_transfer_to_regs, @function
+port_transfer_to_regs:
     movq    %rdi, %rdx // regs pointer (1st param - RDI) -> RDX
 
     movq    0x08(%rdx), %rbp // RBP field
@@ -80,3 +80,38 @@ __skipefl__:
     movq    (%rsp), %rsp     // load new RSP
     jmpq    * -0x88(%rsp)    // JMP to new RIP
 
+
+// void port_longjump_stub(void)
+//
+// after returning from the called function, RSP points to the 2 argument
+// slots in the stack. Saved Registers structure pointer is (RSP + 48)
+//
+// | interrupted |
+// |  program    | <- RSP where the program was interrupted by signal
+// |-------------|
+// | 0x80 bytes  | <- 'red zone' - we will not change it
+// |-------------|
+// | return addr |
+// | from stub   | <- for using in port_transfer_to_regs as [(new RSP) - 128 - 8]
+// |-------------|
+// |    saved    |
+// |  Registers  | <- to restore register context
+// |-------------|
+// | [alignment] | <- align Regs pointer to 16-bytes boundary
+// |-------------|
+// |  pointer to |
+// |  saved Regs | <- (RSP + 128)
+//                // |-------------|
+//                // | 0x80 bytes  | <- 'red zone'
+// |-------------|
+// | return addr |
+// |  from 'fn'  | <- address to return to the port_longjump_stub
+// |-------------|
+
+.globl port_longjump_stub
+	.type	port_longjump_stub, @function
+port_longjump_stub:
+//    movq    128(%rsp), %rcx // load RCX with the address of saved Registers
+    movq    (%rsp), %rcx // load RCX with the address of saved Registers
+    callq   port_transfer_to_regs   // restore context
+    ret                             // dummy RET - unreachable

enhanced/drlvm/trunk/make/vm/port.xml
@@ -38,7 +38,6 @@
             <include name="file_io/linux/*.c" if="is.unix"/>
             <include name="misc/linux/*.c" if="is.unix"/>
             <include name="vmem/linux/*.c" if="is.unix"/>
-            <include name="thread/linux/*.c" if="is.unix"/>
 
             <include name="atomic/linux/*.c" if="is.unix" unless="is.ia64"/>
             <include name="atomic/linux_ipf/*.c" if="is.unix.ia64"/>
@@ -48,13 +47,19 @@
             <include name="file_io/win/*.c" if="is.windows"/>
             <include name="misc/win/*.c" if="is.windows"/>
             <include name="vmem/win/*.c" if="is.windows"/>
-            <include name="thread/win/*.c" if="is.windows"/>
             <include name="file_io/*.c"/>
 
             <include name="modules/*.c"/>
             <include name="modules/linux/*.c" if="is.unix"/>
             <include name="modules/win/*.c" if="is.windows"/>
-            <include name="thread/linux/*ipf.cpp" if="is.unix.ia64"/>
+
+            <include name="thread/linux/*_os.c" if="is.unix"/>
+            <include name="thread/linux/*ia32.c" if="is.unix.x86"/>
+            <include name="thread/linux/*em64t.c" if="is.unix.x86_64"/>
+            <include name="thread/linux/*ipf.c" if="is.unix.ia64"/>
+            <include name="thread/win/*_os.c" if="is.windows"/>
+            <include name="thread/win/*ia32.c" if="is.windows.x86"/>
+            <include name="thread/win/*em64t.c" if="is.windows.x86_64"/>
         </patternset>
 
         <depend-includes paths="port.includes"/>

enhanced/drlvm/trunk/vm/include/open/hythread_ext.h
@@ -153,10 +153,17 @@ extern "C" {
 #include "hycond_win.h"
 #endif // _WIN32
 
-#if defined(__linux__) || defined(FREEBSD) 
+#if defined(__linux__) || defined(FREEBSD)
+
+#include <sys/ucontext.h>
 #define osthread_t pthread_t
+#define thread_context_t ucontext_t
+
 #elif _WIN32
+
 #define osthread_t HANDLE
+#define thread_context_t CONTEXT
+
 #else // !_WIN32 && !__linux__
 #error "threading is only supported on __linux__ or _WIN32"
 #endif // !_WIN32 && !__linux__

enhanced/drlvm/trunk/vm/include/open/ncai_thread.h
@@ -33,16 +33,7 @@
 
 #include "jvmti_types.h"
 #include "hythread_ext.h"
-
-
-#ifdef WIN32
-/* Thread context definition for Windows */
-typedef CONTEXT  os_thread_context_t;
-#else
-/* Thread context definition for UNIX-like systems */
-#include <sys/ucontext.h>
-typedef ucontext_t  os_thread_context_t;
-#endif
+#include "port_thread.h"
 
 
 #ifdef __cplusplus
@@ -62,9 +53,9 @@ extern HY_CFUNC int VMCALL
 hythread_get_suspend_count_native PROTOTYPE((hythread_t thread));
 
 extern HY_CFUNC IDATA VMCALL
-hythread_get_thread_context PROTOTYPE((hythread_t thread, os_thread_context_t* pcontext));
+hythread_get_thread_context PROTOTYPE((hythread_t thread, thread_context_t* pcontext));
 extern HY_CFUNC IDATA VMCALL
-hythread_set_thread_context PROTOTYPE((hythread_t thread, os_thread_context_t* pcontext));
+hythread_set_thread_context PROTOTYPE((hythread_t thread, thread_context_t* pcontext));
 
 //@}
 

enhanced/drlvm/trunk/vm/port/include/port_thread.h
@@ -23,7 +23,31 @@
  * @brief PORT thread support
  */
 
+/* For osthread_t and thread_context_t types */
+#include "open/hythread_ext.h"
 
+
+/* Thread context definition for UNIX-like systems */
+#if defined(LINUX) || defined(FREEBSD) 
+#if defined(LINUX)
+
+#include <sys/types.h>
+#include <linux/unistd.h>
+#include <errno.h>
+
+#ifdef _syscall0
+static _syscall0(pid_t, gettid)/* static definition */
+#else /* _syscall0 */
+#include <sys/syscall.h>
+#include <unistd.h>
+#define gettid() ((pid_t)syscall(__NR_gettid))
+#endif /* _syscall0 */
+
+#else /* !LINUX */
+#define gettid() getpid()
+#endif
+
+#endif /* LINUX || FREEBSD */
 
 /* To skip platform_types.h inclusion */
 typedef struct Registers Registers;
@@ -33,13 +57,70 @@ extern "C" {
 #endif /* __cplusplus */
 
 
-/** @name Threads manipulation and information
+/** @name OS thread operations
  */
 //@{
 
 
+void port_thread_yield_other(osthread_t thread);
+int port_thread_cancel(osthread_t os_thread);
+
+int port_thread_suspend(osthread_t thread);
+int port_thread_resume(osthread_t thread);
+int port_thread_get_suspend_count(osthread_t thread);
+
+int port_thread_get_context(osthread_t thread, thread_context_t* pcontext);
+int port_thread_set_context(osthread_t thread, thread_context_t* pcontext);
+
+void port_thread_context_to_regs(Registers* regs, thread_context_t* context);
+void port_thread_regs_to_context(thread_context_t* context, Registers* regs);
+
 /* Transfer control to specified register context */
-void transfer_to_regs(Registers* regs);
+void port_transfer_to_regs(Registers* regs);
+
+/**
+* Prepares 'Registers' structure and stack area pointed in for calling
+* 'fn' function with a set of arguments provided in variable args list.
+* THe 'fn' function is called through a special stub function with
+* preserving 'red zone' on Linux and clearing direction flag on Windows.
+* After returning from 'fn' and stub, processor registers are restored
+* with a values provided in 'regs' argument.
+* The function can be used to prepare register context for transfering
+* a control to a signal/exception handling function out of the OS handler.
+*
+* When the first argument passed to 'fn' is the same 'regs' pointer, its
+* value is substituted with the pointer stored 'Registers' structure used
+* to restore register context. If 'fn' function modifies the context
+* pointed by the first argument, these changes will take effect after
+* returning from 'fn'.
+*
+* The stub for calling 'fn' is written in assembler language; 'Registers'
+* fields and size are hardcoded. It would be better to rewrite it using
+* encoder in future, to keep control on 'Registers' structure and size.
+*
+* @param [in] fn    - the address of the function to be called
+* @param [in] regs  - the register context
+* @param [in] num   - the number of parameters passed to the 'fn' function
+*                     in the variable args list (6 args at maximum)
+* @param [in] ...   - the parameters for 'fn'; should all be void* or of
+*                     the same size (pointer-sized)
+*/
+void port_set_longjump_regs(void* fn, Registers* regs, int num, ...);
+
+/**
+* The same as 'port_set_longjump_regs', but transfers a control to the
+* prepared registers context by itself.
+* Actually it's a combination of 'port_set_longjump_regs' and
+* 'port_transfer_to_regs' functions, but 'regs' fields are kept unchanged.
+*
+* @param [in] fn    - the address of the function to be called
+* @param [in] regs  - the register context
+* @param [in] num   - the number of parameters passed to the 'fn' function
+*                     in the variable args list (6 args at maximum)
+* @param [in] ...   - the parameters for 'fn'; should all be void* or of
+*                     the same size (pointer-sized)
+*/
+void port_transfer_to_function(void* fn, Registers* regs, int num, ...);
 
 
 

enhanced/drlvm/trunk/vm/port/src/thread/linux/thread_asm_ia32.s
@@ -30,11 +30,11 @@
 // uint32 eflags; +24
 // };
 //
-// void transfer_to_regs(Registers* regs)
+// void port_transfer_to_regs(Registers* regs)
 
-.globl transfer_to_regs
-	.type	transfer_to_regs, @function
-transfer_to_regs:
+.globl port_transfer_to_regs
+	.type	port_transfer_to_regs, @function
+port_transfer_to_regs:
     movl    0x04(%esp), %edx // store regs pointer to EDX
     movl    0x20(%edx), %ebx // EIP field -> EBX
     movl    0x1C(%edx), %ecx // ESP field
@@ -56,3 +56,38 @@ _label_:
     movl    0x0C(%edx), %edx // EDX field
     movl    0x04(%esp), %esp // ((new ESP - 4) -> ESP
     ret                      // JMP by RET
+
+
+// void port_longjump_stub(void)
+//
+// after returning from the called function, EBP points to the pointer
+// to saved Registers structure
+//
+// | interrupted |
+// |  program    | <- ESP where the program was interrupted by signal
+// |-------------|
+// | return addr |
+// | from stub   | <- for using in port_transfer_to_regs
+// |-------------|
+// |    saved    |
+// |  Registers  | <- to restore register context
+// |-------------|
+// |  pointer to |
+// |  saved Regs | <- EBP
+// |-------------|
+// |  arg 5      | <-
+// |-------------|   |
+// ...............    - arguments for 'fn'
+// |-------------|   |
+// |  arg 0      | <-
+// |-------------|
+// | return addr |
+// |  from 'fn'  | <- address to return to the port_longjump_stub
+// |-------------|
+
+.globl port_longjump_stub
+	.type	port_longjump_stub, @function
+port_longjump_stub:
+    movl    %ebp, %esp    // ESP now points to the address of saved Registers
+    call    port_transfer_to_regs   // restore context
+    ret                             // dummy RET - unreachable

enhanced/drlvm/trunk/vm/port/src/thread/linux/thread_em64t.c
@@ -1,0 +1,187 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+#define  _GNU_SOURCE
+#include <sys/ucontext.h>
+#include "port_thread.h"
+
+
+void port_thread_context_to_regs(Registers* regs, ucontext_t *uc)
+{
+    regs->rax = uc->uc_mcontext.gregs[REG_RAX];
+    regs->rcx = uc->uc_mcontext.gregs[REG_RCX];
+    regs->rdx = uc->uc_mcontext.gregs[REG_RDX];
+    regs->rdi = uc->uc_mcontext.gregs[REG_RDI];
+    regs->rsi = uc->uc_mcontext.gregs[REG_RSI];
+    regs->rbx = uc->uc_mcontext.gregs[REG_RBX];
+    regs->rbp = uc->uc_mcontext.gregs[REG_RBP];
+    regs->rip = uc->uc_mcontext.gregs[REG_RIP];
+    regs->rsp = uc->uc_mcontext.gregs[REG_RSP];
+    regs->r8  = uc->uc_mcontext.gregs[REG_R8];
+    regs->r9  = uc->uc_mcontext.gregs[REG_R9];
+    regs->r10 = uc->uc_mcontext.gregs[REG_R10];
+    regs->r11 = uc->uc_mcontext.gregs[REG_R11];
+    regs->r12 = uc->uc_mcontext.gregs[REG_R12];
+    regs->r13 = uc->uc_mcontext.gregs[REG_R13];
+    regs->r14 = uc->uc_mcontext.gregs[REG_R14];
+    regs->r15 = uc->uc_mcontext.gregs[REG_R15];
+    regs->eflags = uc->uc_mcontext.gregs[REG_EFL];
+}
+
+void port_thread_regs_to_context(ucontext_t *uc, Registers* regs)
+{
+    uc->uc_mcontext.gregs[REG_RAX] = regs->rax;
+    uc->uc_mcontext.gregs[REG_RCX] = regs->rcx;
+    uc->uc_mcontext.gregs[REG_RDX] = regs->rdx;
+    uc->uc_mcontext.gregs[REG_RDI] = regs->rdi;
+    uc->uc_mcontext.gregs[REG_RSI] = regs->rsi;
+    uc->uc_mcontext.gregs[REG_RBX] = regs->rbx;
+    uc->uc_mcontext.gregs[REG_RBP] = regs->rbp;
+    uc->uc_mcontext.gregs[REG_RIP] = regs->rip;
+    uc->uc_mcontext.gregs[REG_RSP] = regs->rsp;
+    uc->uc_mcontext.gregs[REG_R8]  = regs->r8;
+    uc->uc_mcontext.gregs[REG_R9]  = regs->r9;
+    uc->uc_mcontext.gregs[REG_R10] = regs->r10;
+    uc->uc_mcontext.gregs[REG_R11] = regs->r11;
+    uc->uc_mcontext.gregs[REG_R12] = regs->r12;
+    uc->uc_mcontext.gregs[REG_R13] = regs->r13;
+    uc->uc_mcontext.gregs[REG_R14] = regs->r14;
+    uc->uc_mcontext.gregs[REG_R15] = regs->r15;
+    uc->uc_mcontext.gregs[REG_EFL] = regs->eflags;
+}
+
+
+void port_longjump_stub(void);
+#define DIR_FLAG ((uint32)0x00000400)
+
+void port_set_longjump_regs(void* fn, Registers* regs, int num, ...)
+{
+    void** sp;
+    va_list ap;
+    int i;
+    size_t align;
+    void** p_pregs;
+    size_t rcount =
+        (sizeof(Registers) + sizeof(void*) - 1) / sizeof(void*);
+
+    if (!regs)
+        return;
+
+    sp = (void**)regs->rsp - 16 - 1; /* preserve 128-bytes 'red zone' */
+    *sp = (void*)regs->rip;
+    align = !((rcount & 1) ^ (((uint64)sp & sizeof(void*)) != 0));
+    p_pregs = sp - rcount - align - 1;
+    sp = sp - rcount;
+    *((Registers*)sp) = *regs;
+    *p_pregs = (void*)sp;
+
+//    sp = p_pregs - 16 - 1; /* preserve 128-bytes 'red zone' */
+    sp = p_pregs - 1; /* set sp to return address */
+
+    va_start(ap, num);
+
+    if (num > 0)
+    {
+        void* arg = va_arg(ap, void*);
+        if (arg == regs)
+            regs->rdi = (uint64)(*p_pregs); /* Replace 1st arg */
+        else
+            regs->rdi = (uint64)arg;
+    }
+
+    if (num > 1)
+        regs->rsi = (uint64)va_arg(ap, void*);
+
+    if (num > 2)
+        regs->rdx = (uint64)va_arg(ap, void*);
+
+    if (num > 3)
+        regs->rcx = (uint64)va_arg(ap, void*);
+
+    if (num > 4)
+        regs->r8 = (uint64)va_arg(ap, void*);
+
+    if (num > 5)
+        regs->r9 = (uint64)va_arg(ap, void*);
+
+    *sp = (void*)&port_longjump_stub;
+    regs->rsp = (uint64)sp;
+    regs->rip = (uint64)fn;
+    regs->eflags = regs->eflags & ~DIR_FLAG;
+}
+
+void port_transfer_to_function(void* fn, Registers* pregs, int num, ...)
+{
+    void** sp;
+    va_list ap;
+    int i;
+    size_t align;
+    void** p_pregs;
+    size_t rcount =
+        (sizeof(Registers) + sizeof(void*) - 1) / sizeof(void*);
+    Registers regs;
+
+    if (!pregs)
+        return;
+
+    regs = *pregs;
+
+    sp = (void**)regs.rsp - 16 - 1; /* preserve 128-bytes 'red zone' */
+    *sp = (void*)regs.rip;
+    align = !((rcount & 1) ^ (((uint64)sp & sizeof(void*)) != 0));
+    p_pregs = sp - rcount - align - 1;
+    sp = sp - rcount;
+    *((Registers*)sp) = regs;
+    *p_pregs = (void*)sp;
+
+//    sp = p_pregs - 16 - 1; /* preserve 128-bytes 'red zone' */
+    sp = p_pregs - 1; /* set sp to return address */
+
+    va_start(ap, num);
+
+    if (num > 0)
+    {
+        void* arg = va_arg(ap, void*);
+        if (arg == pregs)
+            regs.rdi = (uint64)(*p_pregs); /* Replace 1st arg */
+        else
+            regs.rdi = (uint64)arg;
+    }
+
+    if (num > 1)
+        regs.rsi = (uint64)va_arg(ap, void*);
+
+    if (num > 2)
+        regs.rdx = (uint64)va_arg(ap, void*);
+
+    if (num > 3)
+        regs.rcx = (uint64)va_arg(ap, void*);
+
+    if (num > 4)
+        regs.r8 = (uint64)va_arg(ap, void*);
+
+    if (num > 5)
+        regs.r9 = (uint64)va_arg(ap, void*);
+
+    *sp = (void*)&port_longjump_stub;
+    regs.rsp = (uint64)sp;
+    regs.rip = (uint64)fn;
+    regs.eflags = regs.eflags & ~DIR_FLAG;
+
+    port_transfer_to_regs(&regs);
+}

enhanced/drlvm/trunk/vm/port/src/thread/linux/thread_ia32.c
@@ -16,14 +16,14 @@
  */
 
 
+#define  _GNU_SOURCE
 #include <sys/ucontext.h>
-#include <stdio.h>
-#include "vm_core_types.h"
-#include "signals_common.h"
+#include <stdarg.h>
+#include "port_thread.h"
 
 
 #if defined(LINUX)
-void ucontext_to_regs(Registers* regs, ucontext_t *uc)
+void port_thread_context_to_regs(Registers* regs, ucontext_t *uc)
 {
     regs->eax = uc->uc_mcontext.gregs[REG_EAX];
     regs->ecx = uc->uc_mcontext.gregs[REG_ECX];
@@ -37,7 +37,7 @@ void ucontext_to_regs(Registers* regs, ucontext_t *uc)
     regs->eflags = uc->uc_mcontext.gregs[REG_EFL];
 }
 
-void regs_to_ucontext(ucontext_t *uc, Registers* regs)
+void port_thread_regs_to_context(ucontext_t *uc, Registers* regs)
 {
     uc->uc_mcontext.gregs[REG_EAX] = regs->eax;
     uc->uc_mcontext.gregs[REG_ECX] = regs->ecx;
@@ -52,7 +52,7 @@ void regs_to_ucontext(ucontext_t *uc, Registers* regs)
 }
 
 #elif defined(FREEBSD)
-void ucontext_to_regs(Registers* regs, ucontext_t *uc)
+void port_thread_context_to_regs(Registers* regs, ucontext_t *uc)
 {
     regs->eax = uc->uc_mcontext.mc_eax;
     regs->ecx = uc->uc_mcontext.mc_ecx;
@@ -66,7 +66,7 @@ void ucontext_to_regs(Registers* regs, ucontext_t *uc)
     regs->eflags = uc->uc_mcontext.mc_eflags;
 }
 
-void regs_to_ucontext(ucontext_t *uc, Registers* regs)
+void port_thread_regs_to_context(ucontext_t *uc, Registers* regs)
 {
     uc->uc_mcontext.mc_eax = regs->eax;
     uc->uc_mcontext.mc_ecx = regs->ecx;
@@ -83,3 +83,88 @@ void regs_to_ucontext(ucontext_t *uc, Registers* regs)
 #else
 #error need to add correct mcontext_t lookup for registers
 #endif
+
+
+void port_longjump_stub(void);
+#define DIR_FLAG ((uint32)0x00000400)
+
+void port_set_longjump_regs(void* fn, Registers* regs, int num, ...)
+{
+    void** sp;
+    va_list ap;
+    int i;
+    size_t rcount =
+        (sizeof(Registers) + sizeof(void*) - 1) / sizeof(void*);
+
+    if (!regs)
+        return;
+
+    sp = (void**)regs->esp - 1;
+    *sp = (void*)regs->eip;
+    sp = sp - rcount - 1;
+    *((Registers*)(sp + 1)) = *regs;
+    *sp = (void*)(sp + 1);
+    regs->ebp = (uint32)sp;
+
+    sp = sp - num - 1;
+
+    va_start(ap, num);
+
+    for (i = 1; i <= num; i = i + 1)
+    {
+        void* arg = va_arg(ap, void*);
+
+        if (i == 1 && arg == regs)
+            sp[i] = *((void**)regs->ebp); /* Replace 1st arg */
+        else
+            sp[i] = arg;
+    }
+
+    *sp = (void*)&port_longjump_stub;
+    regs->esp = (uint32)sp;
+    regs->eip = (uint32)fn;
+    regs->eflags = regs->eflags & ~DIR_FLAG;
+}
+
+void port_transfer_to_function(void* fn, Registers* pregs, int num, ...)
+{
+    void** sp;
+    va_list ap;
+    int i;
+    size_t rcount =
+        (sizeof(Registers) + sizeof(void*) - 1) / sizeof(void*);
+    Registers regs;
+
+    if (!pregs)
+        return;
+
+    regs = *pregs;
+
+    sp = (void**)regs.esp - 1;
+    *sp = (void*)regs.eip;
+    sp = sp - rcount - 1;
+    *((Registers*)(sp + 1)) = regs;
+    *sp = (void*)(sp + 1);
+    regs.ebp = (uint32)sp;
+
+    sp = sp - num - 1;
+
+    va_start(ap, num);
+
+    for (i = 1; i <= num; i = i + 1)
+    {
+        void* arg = va_arg(ap, void*);
+
+        if (i == 1 && arg == pregs)
+            sp[i] = *((void**)regs.ebp); /* Replace 1st arg */
+        else
+            sp[i] = arg;
+    }
+
+    *sp = (void*)&port_longjump_stub;
+    regs.esp = (uint32)sp;
+    regs.eip = (uint32)fn;
+    regs.eflags = regs.eflags & ~DIR_FLAG;
+
+    port_transfer_to_regs(&regs);
+}

commit 00581cb898ffe507a666596c77b7d461356b3b2f

Applied patch from HARMONY-5576 [drlvm][jvmti] breakpoints are broken by r633414 - regression
svn path=/harmony/; revision=634034

enhanced/drlvm/trunk/vm/port/src/thread/linux/thread_asm_em64t.s
@@ -114,7 +114,7 @@ __skipefl__:
 .globl port_longjump_stub
 	.type	port_longjump_stub, @function
 port_longjump_stub:
-//    movq    128(%rsp), %rcx // load RCX with the address of saved Registers
-    movq    (%rsp), %rcx // load RCX with the address of saved Registers
+//    movq    128(%rsp), %rdi // load RDI with the address of saved Registers
+    movq    (%rsp), %rdi // load RDI with the address of saved Registers
     callq   port_transfer_to_regs   // restore context
     ret                             // dummy RET - unreachable