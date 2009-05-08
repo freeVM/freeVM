freeVM/freeVM

commit f9da03a58bca98c47a47cb6b4f3def3d0d9c9b6a

enlarge half size of buffer to reduce wasted memory
svn path=/harmony/; revision=772849

enhanced/classlib/trunk/modules/luni/src/main/java/java/lang/AbstractStringBuilder.java
@@ -91,8 +91,8 @@ abstract class AbstractStringBuilder {
     }
 
     private void enlargeBuffer(int min) {
-        int twice = (value.length << 1) + 2;
-        char[] newData = new char[min > twice ? min : twice];
+        int newSize = (value.length >> 1 + value.length) + 2;
+        char[] newData = new char[min > newSize ? min : newSize];
         System.arraycopy(value, 0, newData, 0, count);
         value = newData;
         shared = false;

commit 1bd7eba128fd06fc7c6669613bf588bf7688db94

Fix regresstion caused by commit r772849
svn path=/harmony/; revision=772937

enhanced/classlib/trunk/modules/luni/src/main/java/java/lang/AbstractStringBuilder.java
@@ -258,7 +258,8 @@ abstract class AbstractStringBuilder {
      */
     public void ensureCapacity(int min) {
         if (min > value.length) {
-            enlargeBuffer(min);
+            int twice = (value.length << 1) + 2;
+            enlargeBuffer(twice > min ? twice : min);
         }
     }
 