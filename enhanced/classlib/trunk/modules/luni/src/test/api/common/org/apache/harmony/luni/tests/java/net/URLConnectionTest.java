c2a0ecaa9e17fe7b661b60d3838be135ffb9bd57
Mark Hindess <apache@apache.org>
2010-02-18 22:47:56 +0000

    Fixing order of expected and actual so you get better error messages on failure (such as when running on the RI).
    Puzzle of the day: Why does:
    
      test_addRequestPropertyLjava_lang_StringLjava_lang_String
    
    not make any calls to addRequestProperty?  I think this test class needs
    a little attention.
    
    svn path=/harmony/; revision=911630

--- enhanced/classlib/trunk/modules/luni/src/test/api/common/org/apache/harmony/luni/tests/java/net/URLConnectionTest.java
+++ enhanced/classlib/trunk/modules/luni/src/test/api/common/org/apache/harmony/luni/tests/java/net/URLConnectionTest.java
@@ -274,13 +274,13 @@ public class URLConnectionTest extends junit.framework.TestCase {
      */
     public void test_getContentType_regression() throws IOException {
         // Regression for HARMONY-4699
-        assertEquals(getContentType("test.rtf"), "application/rtf");
-        assertEquals(getContentType("test.java"), "text/plain");
+        assertEquals("application/rtf", getContentType("test.rtf"));
+        assertEquals("text/plain", getContentType("test.java"));
         // RI would return "content/unknown"
-        assertEquals(getContentType("test.doc"), "application/msword");
-        assertEquals(getContentType("test.htx"), "text/html");
-        assertEquals(getContentType("test.xml"), "application/xml");
-        assertEquals(getContentType("."), "text/plain");
+        assertEquals("application/msword", getContentType("test.doc"));
+        assertEquals("text/html", getContentType("test.htx"));
+        assertEquals("application/xml", getContentType("test.xml"));
+        assertEquals("text/plain", getContentType("."));
     }
 
     /**
